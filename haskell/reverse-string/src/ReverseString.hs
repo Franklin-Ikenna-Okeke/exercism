93bc245f3460a4d9fb819414f89cd23c54e86633
Franklin <FranklinOkeke_CS@outlook.com>
2025-04-28 07:40:46 +0000

    This is a sencond solution of reversing a string string which is a list of chars in haskell using prepend instead of append which is more efficient

--- haskell/reverse-string/src/ReverseString.hs
+++ haskell/reverse-string/src/ReverseString.hs
@@ -1,5 +1,7 @@
 module ReverseString (reverseString) where
 
 reverseString :: String -> String
-reverseString [] = []
-reverseString (x:xs) = reverseString xs ++ [x]
+reverseString str = helper [] str
+  where
+    helper acc [] = acc
+    helper acc (x:xs) = helper (x:acc) xs
